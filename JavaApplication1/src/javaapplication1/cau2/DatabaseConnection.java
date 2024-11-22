461c643538feb05ea848669754680ef652065749
Duyycoder <duysusi872gmail.com>
2024-11-22 10:56:26 +0700

    update database

--- JavaApplication1/src/javaapplication1/cau2/DatabaseConnection.java
+++ JavaApplication1/src/javaapplication1/cau2/DatabaseConnection.java
@@ -6,7 +6,7 @@ import java.sql.*;
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/qlsv";
     private static final String USER = "root";
-    private static final String PASSWORD = "123456";
+    private static final String PASSWORD = "Daokhuongduy@1";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
